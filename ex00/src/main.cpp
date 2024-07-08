29ef9048067b7647dcf997a5296cbae307221dfe
Batuhan Kas <bkas@k2m02s05.42kocaeli.com.tr>
2024-07-08 14:03:07 +0300

    update

--- ex00/src/main.cpp
+++ ex00/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: bkas <bkas@student.42kocaeli.com.tr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/08 10:51:45 by bkas              #+#    #+#             */
-/*   Updated: 2024/07/08 13:48:59 by bkas             ###   ########.fr       */
+/*   Updated: 2024/07/08 14:02:26 by bkas             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,14 +25,17 @@ int main() {
         const Animal* meta = new Animal();
         const Animal* j = new Dog();
         const Animal* i = new Cat();
+        Cat* c1 = new Cat();
         /* ***** [^] Memory Allocation [^] ***** */
 
         /* ******* [v] Screen Output [v] ******* */
-        cout << WHITE << "Animal's type is: " << BLUE << meta->getType() << RESET
-             << endl;
+        cout << WHITE << "Animal's type is: " << BLUE << meta->getType()
+             << RESET << endl;
         cout << WHITE << "Dog's type is: " << BLUE << j->getType() << RESET
              << endl;
-        cout << WHITE << "Cat's type is: " << BLUE << i->getType() << RESET
+        cout << WHITE << "Cat1's type is: " << BLUE << i->getType() << RESET
+             << endl;
+        cout << WHITE << "Cat2's type is: " << BLUE << c1->getType() << RESET
              << endl;
         /* ******* [^] Screen Output [^] ******* */
 
@@ -40,12 +43,14 @@ int main() {
         meta->makeSound();
         j->makeSound();
         i->makeSound();
+        c1->makeSound();
         /* ******** [^] Make Sound [^] ******** */
 
         /* ******* [v] Delete [v] ******* */
-        delete meta;
-        delete j;
+        delete c1;
         delete i;
+        delete j;
+        delete meta;
         /* ******* [^] Delete [^] ******* */
     } catch (const exception& e) {
         cerr << "Memory Allocation Error: " << e.what() << endl;
